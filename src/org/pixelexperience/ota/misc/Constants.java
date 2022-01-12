f581ec7933031a50bf2aed6c35586fdfa2d18c59
Emanuele Scarlata <scarlataemanuele@gmail.com>
2022-01-12 12:06:23 +0100

    ota: adapt for my "unofficial" build
    if we want to call it a challenge, PE doesn't want to accept my job as an officer
    because they have the device maintainer, but I don't forbid anyone from doing OTAs with my builds.
    
    Thanks to @IAmDeadlylxrd for his ideas, for make it
    
    Signed-off-by: Emanuele Scarlata <scarlataemanuele@gmail.com>

--- src/org/pixelexperience/ota/misc/Constants.java
+++ src/org/pixelexperience/ota/misc/Constants.java
@@ -30,9 +30,9 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://download.pixelexperience.org/ota_v5/%s/%s";
-    static final String OTA_CI_URL = "https://download.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
+    static final String OTA_URL = "https://raw.githubusercontent.com/Fenix46/ota-device/master/pe-12/%s.json";
+    static final String OTA_CI_URL = "@null";
+    static final String MAINTAINER_URL = "https://github.com/Fenix46";
+    static final String DOWNLOAD_WEBPAGE_URL = "https://raw.githubusercontent.com/Fenix46/ota-device/master/pe-12/%s_changelogs.txt";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
